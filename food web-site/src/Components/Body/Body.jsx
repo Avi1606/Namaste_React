2525d8006e51bd614a78a94aeb5482d8584eb6a3
Avikumar <aviiiprajapati16@gmail.com>
2025-04-09 16:11:35 +0530

    logo added

--- food web-site/src/Components/Body/Body.jsx
+++ food web-site/src/Components/Body/Body.jsx
@@ -6,14 +6,41 @@ const body = () =>{
         <div className="body">
             <div className="search-bar">Search</div>
             <div className = "restrorent-card">
-                <RestorentCard />
-                <RestorentCard />
-                <RestorentCard />
-                <RestorentCard />
-                <RestorentCard />
-                <RestorentCard />
-                <RestorentCard />
-
+                <RestorentCard
+                    resName= "BurgerKing"
+                    resRating="3.1 *"
+                    imgUrl="https://logowik.com/content/uploads/images/burger-king-new-20204303.logowik.com.webp"
+                    resDeliveryTime ="20 MIN" />
+                <RestorentCard
+                    resName="McDonald's"
+                    resRating="4.2 *"
+                    imgUrl="https://logowik.com/content/uploads/images/mcdonalds-golden-arches1235.logowik.com.webp"
+                    resDeliveryTime="15 MIN" />
+                <RestorentCard
+                    resName="KFC"
+                    resRating="4.0 *"
+                    imgUrl="https://logowik.com/content/uploads/images/kfc2930.jpg"
+                    resDeliveryTime="25 MIN" />
+                <RestorentCard
+                    resName="Domino's Pizza"
+                    resRating="4.5 *"
+                    imgUrl="https://logowik.com/content/uploads/images/dominos-pizza5190.jpg"
+                    resDeliveryTime="30 MIN" />
+                <RestorentCard
+                    resName="Subway"
+                    resRating="3.8 *"
+                    imgUrl="https://logowik.com/content/uploads/images/subway-20028043.logowik.com.webp"
+                    resDeliveryTime="22 MIN" />
+                <RestorentCard
+                    resName="Pizza Hut"
+                    resRating="4.1 *"
+                    imgUrl="https://logowik.com/content/uploads/images/130_pizzahut.jpg"
+                    resDeliveryTime="35 MIN" />
+                <RestorentCard
+                    resName="Taco Bell"
+                    resRating="3.9 *"
+                    imgUrl="https://logowik.com/content/uploads/images/taco-bell-colored7471.logowik.com.webp"
+                    resDeliveryTime="18 MIN" />
 
             </div>
         </div>
--- food web-site/src/Components/Restorent Card/RestorentCard.jsx
+++ food web-site/src/Components/Restorent Card/RestorentCard.jsx
@@ -1,15 +1,16 @@
 import './RestorentCard.css'
 
-const RestorentCard = () => {
+
+const RestorentCard = (props) => {
     return (
         <div className="restorent-card">
 
             <div className="restro-logo" style={{backgroundColor: "grey"}}>
-                <img src ="https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZGVsaWNpb3VzJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D"/>
+                <img src ={props.imgUrl}/>
             </div>
-            <div className="restro-name">Biriyani Blues</div>
-            <div className = "restro-rating">4.4 *</div>
-            <div className = "restro-delivery-time"> 20 min</div>
+            <div className="restro-name">{props.resName}</div>
+            <div className = "restro-rating">{props.resRating}</div>
+            <div className = "restro-delivery-time">{props.resDeliveryTime}</div>
 
         </div>
     )
