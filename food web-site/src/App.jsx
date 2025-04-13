d47bf43d0ceb7fec79e49333394c2062dc59ecf5
Avikumar <aviiiprajapati16@gmail.com>
2025-04-13 16:13:05 +0530

    Router inigrated

--- food web-site/src/App.jsx
+++ food web-site/src/App.jsx
@@ -1,19 +1,37 @@
 import Header from './Components/Header/Header.jsx';
-import Body from './Components/Body/Body.jsx';
-import './App.css'
+import './App.css';
 import Body_swiggy from "./Components/Body(Swiggy)/Body_swiggy.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutPage from "./Components/About.jsx";
 
-function App() {
-  return (
-      <div className="app">
-        <div className="header">
-          <Header />
+function AppLayout() {
+    return (
+        <div className="app">
+            <div className="header">
+                <Header/>
+            </div>
+            <div className="body">
+                <Body_swiggy/>
+            </div>
         </div>
-        <div className="body">
-          <Body_swiggy/>
-        </div>
-      </div>
-  )
+    )
 }
 
-export default App
+// Create router with both paths
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />
+    },
+    {
+        path: "/about",
+        element: <AboutPage />
+    }
+]);
+
+// Main App component
+function App() {
+  return <RouterProvider router={appRouter} />;
+}
+
+export default App;
---
+++ food web-site/src/Components/About.jsx
@@ -0,0 +1,12 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <div className="content">
+        <h1>About page</h1>
+        <h2>This is about page of food website</h2>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
