68a1520901a8366fb48d7d06f4b3a74e86c05caa
Avikumar <aviiiprajapati16@gmail.com>
2025-04-13 10:59:55 +0530

    login button added

--- food web-site/src/Components/Body(Swiggy)/Body_swiggy.jsx
+++ food web-site/src/Components/Body(Swiggy)/Body_swiggy.jsx
@@ -1,10 +1,26 @@
 import './Body_swiggy.css';
 import RestorentCard from "../Restorent Card (Swiggy)/RestorentCard.jsx";
 import {restodata} from "../Utils/Data.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 const Body_swiggy = () => {
     const[resdata, setResdata] = useState(restodata);
+
+    // useEffect(() => {
+    //     fetchData();
+    // }, []);
+    //
+    // const fetchData = async () => {
+    //     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.450876021041882&lng=77.58510968186796&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    //
+    //     const json = await data.json();
+    //     // console.log();
+    //     setResdata(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    // }
+
+    if (restodata.length === 0) {
+        return <h2>Loading...</h2>;
+    }
 
     return (
         <div className="body">
--- food web-site/src/Components/Header/Header.jsx
+++ food web-site/src/Components/Header/Header.jsx
@@ -1,8 +1,15 @@
 
 import './Header.css'
 import {LOGO} from "../Utils/Constants.jsx";
+import {useState} from "react";
+
+
 
 const Header = () => {
+
+    const[loginButton, setLoginButton] = useState("Login");
+
+    if (loginButton === "Login") {}
     return (
         <div className="header">
             <div className="logo">
@@ -15,6 +22,14 @@ const Header = () => {
                         <li>About</li>
                         <li>Contact</li>
                         <li>Cart</li>
+                        <button className = "login-button"
+                                onClick ={() =>
+                                    loginButton ==="Login" ?
+                                        setLoginButton("Logout") :
+                                        setLoginButton("Login") }
+                        >
+                            {loginButton}
+                        </button>
                     </ul>
                 </div>
             </div>
