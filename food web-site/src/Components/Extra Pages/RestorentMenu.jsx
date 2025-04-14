b13409fe688d60522ba0ac11d33ef42b3fbad304
Avikumar <aviiiprajapati16@gmail.com>
2025-04-14 18:59:35 +0530

    restorent page added

--- food web-site/src/Components/Extra Pages/RestorentMenu.jsx
+++ food web-site/src/Components/Extra Pages/RestorentMenu.jsx
@@ -1,8 +1,26 @@
+import {useEffect, useState} from "react";
+
+
 const RestorentMenu = () =>{
+
+    const[resInfo , setResInfo] = useState({})
+
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.450876021041882&lng=77.58510968186796&restaurantId=642963&catalog_qa=undefined&submitAction=ENTER");
+
+        const json = await data.json();
+        setResInfo(json.data.cards[2].card.card.info);
+    }
+
     return (
         <div className="RestorentMenu">
             <div>
-                <h1>Restorent Name</h1>
+                <h1>{resInfo.name}</h1>
                 <h3>Food Menu</h3>
             </div>
             <div>
